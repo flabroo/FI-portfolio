3aa53bb4c96a4ee263fefc4ccc2156a3f5b9fd71
Fairul Izwan <izwanrul11@gmail.com>
2023-05-30 15:16:37 +0800

    changed meta pic

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "projects#home"
-  get "about", to: "projects#about"
-  get 'download_cv', to: "projects#download_cv"
-  post 'bot_message', to: "projects#bot_message"
+  root 'projects#home'
+  get 'about', to: 'projects#about'
+  get 'fairul_izwan_cv', to: 'projects#fairul_izwan_cv'
+  post 'bot_message', to: 'projects#bot_message'
 
   resources :projects
 end
