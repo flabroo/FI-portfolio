d72b39150bcfa905c52c0436061961ac171a0841
Fairul Izwan <izwanrul11@gmail.com>
2023-06-05 11:32:03 +0800

    rubocop fix

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -2,9 +2,9 @@ class FilesController < ApplicationController
   def fairul_izwan_cv
     send_file(
       "#{Rails.root}/public/docs/Fairul-CV.pdf",
-      filename: "Fairul-CV.pdf",
-      type: "application/pdf",
-      disposition: "inline"
+      filename: 'Fairul-CV.pdf',
+      type: 'application/pdf',
+      disposition: 'inline'
     )
   end
 end
